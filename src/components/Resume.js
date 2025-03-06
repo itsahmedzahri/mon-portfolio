afb09d2456e8c522c11717d121e584f03ca8fd80
unknown <zahriahmeed@gmail.com>
2025-03-06 00:35:43 +0000

    new commit

---
+++ src/components/Resume.js
@@ -0,0 +1,252 @@
+import Image from "next/image";
+import React from "react";
+import { Timeline } from "@/components/ui/timeline";
+
+export function Resume() {
+  const data = [
+    {
+      title: "2020-2021",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-bold mb-8">
+            Sidi Ou Sidi High School
+          </p>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Baccalaureate in Physical Sciences
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/Lyce.jpg"
+              alt="startup template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "2023-Present",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-bold mb-8">
+            Specialized Institute of Applied Technology, Taroudant
+          </p>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            TECHNICIAN SPECIALIZED IN COMPUTER SYSTEMS AND NETWORKS
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/ISTA.jpg"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/ofppt.jpg"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "2023-Present",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-bold mb-8">
+            Specialized Institute of Applied Technology, Taroudant
+          </p>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+            Entrepreneurial Innovation Program Certificate: From idea to viable project
+          </p>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/pie.jpg"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/IS.jpg"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+              <Image
+              src="/ISTA.jpg"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+      ),
+    },
+    {
+      title: "Interships",
+    },{
+      title: "Nov 2023 - Dec 2023",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-bold mb-4">
+            Technical Internship
+          </p>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
+            Prefecture of Taroudant
+          </p>
+          <div className="mb-8">
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Scan and Verification
+            </div>
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Input
+            </div>
+            
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/pref.jpg"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+    )},
+    
+    {
+      title: "Sep 2024 - Present",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-bold mb-4">
+            Technical Internship
+          </p>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
+            Specialized Institute of Applied Technology of Taroudant
+          </p>
+          <div className="mb-8">
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Installation and configuration of different operating systems
+            </div>
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ System Administration in Windows Server
+            </div>
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Hardware and Software Maintenance 
+            </div>
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Access Point Configuration
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/ISTA.jpg"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/Hpe.jpg"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/Raid.jpg"
+              alt="bento template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/AD.jpg"
+              alt="cards template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+    )},
+    {
+      title: "Mar 2025 - Present",
+      content: (
+        <div>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-base font-bold mb-4">
+            Technical Internship
+          </p>
+          <p
+            className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
+            Marsa Maroc of Agadir
+          </p>
+          <div className="mb-8">
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Switch Configuration
+            </div>
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ System Administration in Windows Server
+            </div>
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Hardware and Software Maintenance 
+            </div>
+            <div
+              className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm">
+              ✅ Access Point Configuration
+            </div>
+          </div>
+          <div className="grid grid-cols-2 gap-4">
+            <Image
+              src="/Marsa-Maroc.jpg"
+              alt="hero template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/ma.png"
+              alt="feature template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/ana .jpg"
+              alt="bento template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+            <Image
+              src="/SA.jpg"
+              alt="cards template"
+              width={500}
+              height={500}
+              className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]" />
+          </div>
+        </div>
+    )},
+  ];
+  return (
+    (<div id="Resume" className="w-full">
+      <Timeline data={data} />
+    </div>)
+  );
+}
